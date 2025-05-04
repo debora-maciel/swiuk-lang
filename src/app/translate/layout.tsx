ef8320955aa735518c189c40104330ddffbf2068
Debora Leticia Ferreira Maciel <debora.ferreira@mplantech.com.br>
2025-05-04 17:15:07 -0300

    upping

--- src/app/translate/layout.tsx
+++ src/app/translate/layout.tsx
@@ -1,3 +1,5 @@
+import Link from "next/link";
+
 export default function TranslateLayout({
     children,
 }: {
@@ -5,7 +7,7 @@ export default function TranslateLayout({
 }) {
     return (
         <div className="bg-orange-100 w-full h-screen flex flex-col items-center justify-center">
-            <div className="pl-5 text-xl pt-2 montserrat-black w-full text-left border-t-6 border-orange-400">Swiuk Lang</div>
+            <Link href={'/'} className="pl-5 text-xl pt-2 montserrat-black w-full text-left border-t-6 border-orange-400">Swiuk Lang</Link>
             {children}
         </div>
     );
--- src/app/translate/page.tsx
+++ src/app/translate/page.tsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from "react";
 // import data from "./data/english.json";
 import data from "./data/words.json";
 import { FaCircleCheck } from "react-icons/fa6";
+import { GrStatusUnknown } from "react-icons/gr";
 import mw from "../../../public/mw.png";
 import dc from "../../../public/dictionarydotcom.png";
 import { FcGoogle } from "react-icons/fc";
@@ -45,19 +46,17 @@ export default function TranslatePage() {
 
     const wordData = getWordProps(data, words[currentWord]);
 
-    console.log("Word Data:", wordData);
-
     const styleCorrect = isCorrect === null
         ? "text-orange-800 bg-orange-100 border-t-0 rounded-[20px] border-r-0 border-10 border-orange-800 max-w-max"
         : isCorrect === true
-            ? "text-green-800 bg-green-100  transform transition duration-300 ease-in-out scale-110"
-            : "text-red-800 border-red-300 ";
+            ? "text-green-800 bg-green-100 border-t-0 rounded-[20px] border-r-0 border-10 border-green-800 z-100 transition-all transition-discrete hidden"
+            : "text-red-800 bg-red-100 rounded-[20px] border-t-0 border-r-0 border-10 border-red-800 z-100 transition-all transition-discrete hidden";
 
     const styleBorderCorrect = isCorrect === null
         ? "max-w-max"
         : isCorrect === true
-            ? "translate-x-full opacity-0"
-            : "-translate-x-full opacity-0";
+            ? " transition-discrete transition-all"
+            : " transition-discrete transition-all";
 
     function handleCorrect() {
         setIsCorrect(true);
@@ -66,7 +65,7 @@ export default function TranslatePage() {
             setIsCorrect(null);
             setCurrentWord((prev) => (prev + 1) % words.length);
             setCorrectWords((prev) => prev + 1);
-        }, 1000);
+        }, 400);
     }
 
     function handleIncorrect() {
@@ -75,10 +74,11 @@ export default function TranslatePage() {
         setTimeout(() => {
             setIsCorrect(null);
             setCurrentWord((prev) => (prev + 1) % words.length);
-        }, 1000);
+        }, 200);
     }
 
-    if (words.length === 0) return null; // or loading state
+    if (words.length === 0) return null;
+
     return (
         <div className="bg-orange-100 w-full h-full xl:rounded-lg flex items-center justify-center p-4">
             <div className="flex flex-col justify-start items-start w-full h-full">
@@ -86,8 +86,37 @@ export default function TranslatePage() {
                     <div className="bg-green-100 border border-b-5 border-l-5 border-r-0 text-green-900 montserrat-semibold rounded-lg p-1">{correctWords}</div>
                     <div className="bg-orange-50 border-t border-b-5 border border-l-0 text-orange-900 montserrat-bold rounded-lg p-1">{words.length}</div>
                 </div>
-                <div className="mx-auto flex items-end gap-1 w-full h-[20%] bg-white rounded-lg py-4 shadow-md">
-                    <div className={styleBorderCorrect + " mx-auto py-3 p-4 rounded-xl transition-all duration-700 ease-in transform max-w-max"}>
+                <div className={`${isCorrect === null ? "bg-white" : isCorrect ? "bg-green-400" : "bg-red-400"}` + " mx-auto flex items-end gap-1 w-full h-[24%] rounded-lg shadow-md relative z-0"}>
+                    <div className="text-orange-200/30 flex flex-wrap absolute w-full h-full z-0 overflow-hidden">
+                        {[
+                            "rotate-350 top-10 left-10 text-[100px] z-0",
+                            "rotate-350 top-2 left-2 text-[20px] z-0 ",
+                            "rotate-10 top-7 left-5 text-[20px] z-0",
+                            "rotate-345 top-3 right-0 text-[40px] z-0",
+                            "rotate-345 bottom-1 left-1 text-[20px] z-0",
+                            "rotate-20 bottom-1 left-36 text-[40px] z-0",
+                            "rotate-345 bottom-1 left-48 text-[20px] z-0",
+                            "rotate-23 bottom-0 left-57 text-[50px] z-0",
+                            "rotate-345 bottom-1 right-15 text-[15px] z-0",
+                            "rotate-12 top-2 left-12 text-[30px] z-0 ",
+                            "rotate-12 top-1 left-50 text-[30px] z-0 ",
+                            "rotate-12 top-30 left-5 text-[30px] z-0 ",
+                            "rotate-12 top-14 right-0 text-[30px] z-0 ",
+                            "rotate-345 top-14 right-10 text-[25px] z-0 ",
+                            "rotate-12 top-12 left-0 text-[30px] z-0 ",
+                            "rotate-12 top-1 right-13 text-[45px] z-0",
+                            "rotate-0 top-1 right-25 text-[20px] z-0",
+                            "rotate-0 top-1 right-40 text-[20px] z-0",
+                            "rotate-0 right-47 text-[40px] z-0",
+                            "rotate-0 top-1 right-60 text-[20px] z-0",
+                        ].map((_, index) => (
+                            <div className={_ + " absolute"} key={index}>
+                                <GrStatusUnknown />
+                            </div>
+                        ))
+                        }
+                    </div>
+                    <div className={styleBorderCorrect + " mx-auto py-3 p-4 rounded-xl transition-all duration-50 ease-in transform max-w-max z-100"}>
                         <h1 className={styleCorrect + " rounded shadow-md text-xl xl:text-5xl font-bold px-10 py-4 capitalize"}>
                             {words[currentWord]}
                         </h1>
@@ -101,8 +130,8 @@ export default function TranslatePage() {
                         <FaCircleCheck />
                     </button>
                 </div>
-                <div className="w-full p-1 mt-4 h-full">
-                    <div className="bg-orange-200/70 rounded-lg p-4 shadow-md border-dashed border-3 border-orange-300">
+                <div className="w-full p-1 mt-4 max-h-[40%] min-h-[40%]">
+                    <div className="bg-orange-200/70 rounded-lg p-4 shadow-md border-dashed border-3 border-orange-300 h-full overflow-y-scroll">
                         {wordData?.MEANINGS.map((meaning, index) => (
                             <div key={index} className="text-sm xl:text-lg text-orange-700 mb-3">
                                 <div className="font-bold text-orange-800">[{meaning[0]}]</div>
@@ -113,13 +142,13 @@ export default function TranslatePage() {
                         <div className="text-sm xl:text-lg font-bold text-orange-800">[Synonyms]</div>
                         <div className="text-sm xl:text-lg text-gray-700 mb-3 flex pt-1 flex flex-wrap gap-2">
                             {wordData?.SYNONYMS.map((syn, index) => (
-                                <div className="border rounded-lg bg-orange-300/30 text-red-800 text-xs border-orange-800/20 px-2 w-max" key={index}>{syn}</div>
+                                <a href={"https://www.google.com/search?q=" + syn} className="border rounded-lg bg-orange-300/30 text-red-800 text-xs border-orange-800/20 px-2 w-max" key={index}>{syn}</a>
                             ))}
                         </div>
                     </div>
                 </div>
                 <div className="text-xs w-2/3 mt-6 mb-2 font-bold text-red-900 pl-4">Tap to learn more.</div>
-                <div className="w-full flex items-center justify-start gap-2 pl-4">
+                <div className="w-full flex items-center justify-start gap-2 pl-4 ">
                     <a href={"https://www.google.com/search?q=" + words[currentWord]} target="_blank" rel="noopener noreferrer">
                         <button
                             className="cursor-pointer text-white rounded-full text-[25px]">
