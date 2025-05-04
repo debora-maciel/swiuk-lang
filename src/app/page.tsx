42730e2296d9af1b0b5b4b64b33191dab28cf61b
Debora Leticia Ferreira Maciel <debora.ferreira@mplantech.com.br>
2025-05-04 13:33:17 -0300

    upping correction

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,12 +6,12 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div className="w-full h-screen bg-orange-100">
-      <div className="flex items-center justify-center h-full gap-20">
+      <div className="flex items-center justify-center h-full gap-10 xl:gap-20 xl:flex-row flex-col">
         <Link href={'/translate'} className="cursor-pointer bg-orange-900 text-white p-2 rounded-full shadow-xl flex items-center justify-center">
           <div className="bg-orange-50 text-white p-4 rounded-full shadow-xl flex items-center justify-center">
             <div className="bg-orange-400 text-white p-7 rounded-full shadow-xl flex items-center justify-center">
-              <div className="bg-orange-700 text-white p-4 rounded-full shadow-lg flex items-center justify-center">
-                <TiSortAlphabeticallyOutline size={120} />
+              <div className="bg-orange-700 text-white p-4 rounded-full shadow-lg flex items-center justify-center text-[40px] xl:text-[120px]">
+                <TiSortAlphabeticallyOutline />
               </div>
             </div>
           </div>
@@ -19,8 +19,8 @@ export default function Home() {
         <div className="cursor-pointer hover:scale-2 bg-yellow-900 text-white p-2 rounded-full shadow-xl flex items-center justify-center">
           <div className="bg-yellow-50 text-white p-4 rounded-full shadow-xl flex items-center justify-center">
             <div className="bg-yellow-400 text-white p-7 rounded-full shadow-xl flex items-center justify-center">
-              <div className="bg-yellow-700 text-white p-4 rounded-full shadow-lg flex items-center justify-center">
-                <IoGameController size={120} />
+              <div className="bg-yellow-700 text-white p-4 rounded-full shadow-lg flex items-center justify-center text-[40px] xl:text-[120px]">
+                <IoGameController />
               </div>
             </div>
           </div>
@@ -28,8 +28,8 @@ export default function Home() {
         <div className="cursor-pointer hover:scale-2 bg-blue-900 text-white p-2 rounded-full shadow-xl flex items-center justify-center">
           <div className="bg-blue-50 text-white p-4 rounded-full shadow-xl flex items-center justify-center">
             <div className="bg-blue-400 text-white p-7 rounded-full shadow-xl flex items-center justify-center">
-              <div className="bg-blue-700 text-white p-4 rounded-full shadow-lg flex items-center justify-center">
-                <MdOutlineGTranslate size={120} />
+              <div className="bg-blue-700 text-white p-4 rounded-full shadow-lg flex items-center justify-center text-[40px] xl:text-[120px]">
+                <MdOutlineGTranslate />
               </div>
             </div>
           </div>
